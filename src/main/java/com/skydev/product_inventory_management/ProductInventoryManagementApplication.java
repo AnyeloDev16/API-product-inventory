4a69e938c0c356cfd01a42aca6c759ced903fa71
Anyelo Nuñez <anyelonuob2004@gmail.com>
2024-11-10 19:44:55 -0500

    chore: Inicializar proyecto Spring boot

---
+++ src/main/java/com/skydev/product_inventory_management/ProductInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.skydev.product_inventory_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skydev/product_inventory_management/ProductInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skydev.product_inventory_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
